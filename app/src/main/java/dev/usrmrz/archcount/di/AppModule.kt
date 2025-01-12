36f1cc69a02df078f425673f4d900aad26fe6018
usrmrz <usrmrz@gmail.com>
2025-01-12 12:18:28 +0200

    restruct module

--- app/src/main/java/dev/usrmrz/archcount/di/AppModule.kt
+++ app/src/main/java/dev/usrmrz/archcount/di/AppModule.kt
@@ -3,15 +3,23 @@ package dev.usrmrz.archcount.di
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.components.SingletonComponent
+import dev.usrmrz.archcount.feature_state.data.repository.CounterRepositoryImpl
 import dev.usrmrz.archcount.feature_state.domain.repository.CounterRepository
 import dev.usrmrz.archcount.feature_state.domain.use_case.CountUseCases
 import dev.usrmrz.archcount.feature_state.domain.use_case.IncrementCountUseCase
 import dev.usrmrz.archcount.feature_state.domain.use_case.ResetCountUseCase
+import javax.inject.Singleton
 
 @Module
-@InstallIn(ViewModelComponent::class)
-object UseCaseModule {
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideCounterRepository(): CounterRepository {
+        return CounterRepositoryImpl()
+    }
 
     @Provides
     fun provideCountUseCases(repository: CounterRepository): CountUseCases {
@@ -20,5 +28,4 @@ object UseCaseModule {
             reset = ResetCountUseCase(repository)
         )
     }
-
 }
